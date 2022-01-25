ec86a9afadd34c19344283dc5d6a6ce46f0bba1d
Jason Boudreaux <js.bdrx@gmail.com>
2022-01-25 17:05:14 -0600

    added POST.js to models folder

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
-const User = require('./User');
+const User = require("./User");
+const Post = require("./Post");
 
-module.exports = { User };
+module.exports = { User, Post };
